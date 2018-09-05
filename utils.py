bcdeb444e5cbaf57849dcfc249c61b4d4fb1575a
schopra1989 <sahibac89@gmail.com>
2018-09-05 17:11:05 +0530

    Add existing file

---
+++ utils.py
@@ -0,0 +1,29 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+
+
+class ColumnSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        assert isinstance(X, pd.DataFrame)
+
+        try:
+            return X[self.columns]
+        except KeyError:
+            cols_error = list(set(self.columns) - set(X.columns))
+            raise KeyError("The DataFrame does not include the columns: %s" % cols_error)
+
+            
+class TypeSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, dtype):
+        self.dtype = dtype
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        assert isinstance(X, pd.DataFrame)
+        return X.select_dtypes(include=[self.dtype])
